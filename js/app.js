1bc3404f36dfdca4f860ffca91a0b0a481d5009c
Leandrox77 <leandro.ln.dev@gmail.com>
2022-03-29 21:15:48 -0300

    add advice API

---
+++ js/app.js
@@ -0,0 +1,38 @@
+'use strict';
+document.addEventListener('DOMContentLoaded', () => {
+    fetchData();
+})
+
+const fetchData = async () => {
+    try {
+        const getResult = await fetch('https://api.adviceslip.com/advice');
+        const data = await getResult.json()
+        console.log(data);
+
+        const advice = {
+            id: data.slip.id,
+            text: data.slip.advice,
+        }
+
+        showAdvice(advice);
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+const showAdvice = (advice) => {
+    console.log(advice);
+    const container = document.getElementById('container');
+    const template = document.getElementById('templateAdvice').content;
+    const templateClone = template.cloneNode(true);
+    const templateFragment = document.createDocumentFragment();
+    templateClone.getElementById('idAdvice').textContent = `#${advice.id}`
+    console.log(templateClone)
+    templateClone.getElementById('textAdvice').textContent = advice.text;
+    templateFragment.appendChild(templateClone);
+    container.appendChild(templateFragment)
+
+    document.getElementById('newAdvice').addEventListener('click', () => {
+        location.reload()
+    });
+}
